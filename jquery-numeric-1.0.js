1e84f7f98eacd7fcf12f4fb6b1e074dd9c6145c9
roshnikanta <roshbrahm@users.noreply.github.com>
2014-04-13 14:32:37 +0530

    Update jquery-numeric-1.0.js
    embed numeric pad

--- jquery-numeric-1.0.js
+++ jquery-numeric-1.0.js
@@ -4,20 +4,100 @@
 * DESCRIPTION
 * jquery.decimal.js
 * A plugin for input value which restricts user to input decimal value only.
-* it allows decimal value only.
-* it takes an argument in config which indicates the max-character length of the input
-* in the given html, we have put the max-character length as 10.
-* so it will restrict the user to input not more than 10 character.
+* This plugin has an option to embed Numeric Pad.
 * This plugin is free to use and distribute.
 * And anybody willing to upgrade or enhance the plugin are welcome.
 */
 (function($) {
-	$.fn.numeric = function(config, callback){
+    $.fn.numeric = function(config, callback){
 		config = config || {};
-		this.data("limit",config.limit);
+        var embedNumpad=config.numpad;
+        if(embedNumpad==undefined){
+            embedNumpad=false;
+        }
+        var numpadId=$(this).attr("id");
+        if(embedNumpad){
+			var keypad="<br/><div class='numericKeypad'>";
+            keypad+="<div><span style='font-size:8px;text-align:center;color:#fff;'>rNumPad v.1.0</span>"
+            keypad+="<span class='togglePad_"+numpadId+" circle' title='Toggle Numpad.'style='float:right;cursor:pointer;color:#fff;'>-</span>";
+            keypad+="</div>"
+            keypad+="<div id='numpadContent_"+numpadId+"'>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key big-key key' id='"+numpadId+"_backspace'>Backspace</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_7'>7</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_8'>8</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_9'>9</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_4'>4</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_5'>5</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_6'>6</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_1'>1</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_2'>2</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_3'>3</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_0'>0</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_dot'>.</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_minus'>-</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_left'>&#8592;</span>";
+			keypad+="<span class='"+numpadId+"_key key' id='"+numpadId+"_right'>&#8594;</span>";
+			keypad+="</div>";
+			keypad+="<div class='row'>";
+			keypad+="<span class='"+numpadId+"_key big-key key' id='"+numpadId+"_clear'>Clear All</span>";
+			keypad+="</div>";
+			keypad+="</div>";
+            keypad+="</div>";
+			$(this).after(keypad);
+            $("."+numpadId+"_key").click(function(){
+                var max_len=config.limit;  
+                var targetId=$(this).attr("id").split("_")[0];
+                var id=$(this).attr("id").split("_")[1];
+                if(id=="backspace"){
+					backspace(targetId);
+				}
+				if(id=="clear"){
+					clearData(targetId)
+				}
+				if(id=="left"){
+					left(targetId);
+				}
+				if(id=="right"){
+					right(targetId);
+				}
+				if(id=="dot"){
+					setDot(targetId,max_len);
+				}
+				if(id=="minus"){
+					negate($(this).text(),targetId,max_len);
+				}
+				if(!isNaN(id)){
+					setNum($(this).text(),targetId,max_len);
+				}
+
+			});
+            $(".togglePad_"+numpadId).click(function(){
+                $("#numpadContent_"+numpadId).toggle();
+                if($(this).html()=="-"){
+                    $(this).html("+");
+                }
+                else{
+                    $(this).html("-")
+                }
+            })
+
+		}
+        this.data("limit",config.limit);
 		this.data("negative",config.negative);
 		this.data("decimal",config.decimal);
-		$(this).keypress($.fn.numeric.keypress)
+		$(this).keypress($.fn.numeric.keypress);
+
 
 	}
 	$.fn.numeric.keypress = function(event){
@@ -82,24 +162,122 @@
         }
         return true;		
 	}
-	function caret(node) {
-	    if(node.selectionStart) {
-	        return node.selectionStart;
+	function setNum(val,targetId,maxLen){
+	    var answer = document.getElementById(targetId);
+	    var caretPosition=caret(answer);
+        var result = answer.value.splice( caretPosition, 0, val );
+        if(maxLen==undefined){
+            answer.value=result;    
+        }
+	    else if(result.length<=maxLen-1){
+	        answer.value=result;
 	    }
-	    else if(!document.selection) {
-	        return 0;
-	    }
-	    //node.focus();
-	    var c		= "\001";
-	    var sel	= document.selection.createRange();
-	    var txt	= sel.text;
-	    var dul	= sel.duplicate();
-	    var len	= 0;
-	    try{ dul.moveToElementText(node); }catch(e) { return 0; }
-	    sel.text	= txt + c;
-	    len		= (dul.text.indexOf(c));
-	    sel.moveStart('character',-1);
-	    sel.text	= "";
-	    return len;
+	    setCaretPosition(targetId, caretPosition+1);
 	}
+    function negate(val,targetId,maxLen){
+        var answer = document.getElementById(targetId);
+        console.log(targetId)
+        var caretPosition=caret(answer);
+        if(caretPosition==0){
+            if(answer.value.charAt(0)!="-"){
+                if(maxLen==undefined){
+                    answer.value = val+answer.value;   
+                }
+                else if(answer.value.length <=maxLen-1){
+                    answer.value = val+answer.value;
+                }
+                setCaretPosition(targetId, caretPosition+1);
+            }
+        }
+        caretPosition=caret(answer);
+        setCaretPosition(targetId, caretPosition);
+    }
+    function setDot(targetId,maxLen){
+        var answer = document.getElementById(targetId);
+        var caretPosition=caret(answer);
+        if(answer.value.indexOf(".") == -1){
+            var result = answer.value.splice( caretPosition, 0, "." );
+            if(maxLen==undefined){
+                answer.value=result;
+            }
+            else if(result.length <=maxLen){
+                answer.value=result;
+            }
+        }    
+        setCaretPosition(targetId, caretPosition+1)
+    }
+    function backspace(targetId){
+        var answer = document.getElementById(targetId);
+        var caretPosition=caret(answer);
+        answer.value = answer.value.replaceAt(caretPosition-1,"");
+        setCaretPosition(targetId, caretPosition-1)
+    }
+    function left(targetId){
+        var answer = document.getElementById(targetId);
+        var caretPosition=caret(answer);
+        setCaretPosition(targetId, caretPosition-1)
+    }
+    function right(targetId){
+        var answer = document.getElementById(targetId);
+        var caretPosition=caret(answer);
+        setCaretPosition(targetId, caretPosition+1)
+    }
+    function clearData(targetId){
+        var answer = document.getElementById(targetId);
+        answer.value="";
+        setCaretPosition(targetId,0)
+    }
+    function caret(node) {
+        if(node.selectionStart) {
+            return node.selectionStart;
+        }
+        else if(!document.selection) {
+            return 0;
+        }
+        //node.focus();
+        var c		= "\001";
+        var sel	= document.selection.createRange();
+        var txt	= sel.text;
+        var dul	= sel.duplicate();
+        var len	= 0;
+        try{ dul.moveToElementText(node); }catch(e) { return 0; }
+        sel.text	= txt + c;
+        len		= (dul.text.indexOf(c));
+        sel.moveStart('character',-1);
+        sel.text	= "";
+        return len;
+    }
+
+    function  setCaretPosition(elemId, caretPos) {
+        var elem = document.getElementById(elemId);
+        if(elem != null) {
+            if(elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.move('character', caretPos);
+                range.select();
+            }
+            else {
+                if(elem.selectionStart) {
+                    elem.focus();
+                    elem.setSelectionRange(caretPos, caretPos);
+                }
+                else{
+                    elem.focus();
+                    elem.setSelectionRange(caretPos, caretPos);
+                }
+
+            }
+        }
+    }
+
+    String.prototype.splice = function( idx, rem, s ) {
+        return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+    };
+    String.prototype.replaceAt=function(index, character) {
+        return this.substring(0, index) + character + this.substring(index+ 1);
+    };
+    String.prototype.splitAt=function(index){
+        return this.substring(0, index) + "." + this.substring(index);    
+    };
+
 })(jQuery);
